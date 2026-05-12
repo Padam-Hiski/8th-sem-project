496c93e8f7b7f1ad8ee80c9d1c2f2b24046e4ffd
Padam Hiski <prabin@btlkalikacampus.edu.np>
2026-05-12 15:26:05 +0545

    HistorY fix

--- frontend/src/pages/History.jsx
+++ frontend/src/pages/History.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { useApp } from '../AppContext';
 
-const API_URL = 'http://localhost:5000';
+const API_URL = 'https://8th-sem-project-production.up.railway.app';
 
 const text = {
   en: {
